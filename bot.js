2749b530cf86a7387c5628c44aa1f911e8e20ad1
Alessandro <a.buonerba@hotmail.com>
2020-02-24 21:26:13 +0000

    More Messages

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const messages = ['Che schifo di champ', 'Oid', 'Amir, you are so fucking shit', 'I am good with Zed'];
+const messages = ['Che schifo di champ', 'Oid', 'Amir, you are so fucking shit', 'I am good with Zed', 'I do not need Support items', 'I am not drunk', 'Alessaaaaaaaaa', 'Guys, your BOT is MID!!!', 'DO NOT FARM MY MINIONS!!!!', 'DO NOT PUSH MY LANEEE!!!', 'I just woke up', 'HE IS FULL LIFE!!!'];
 
 client.on('ready', () => {
     console.log('I am ready!');
