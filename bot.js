4255fed084c885747772ed96411be7d8213b429d
Alessandro <a.buonerba@hotmail.com>
2020-02-24 14:07:57 +0000

    first commit

--- bot.js
+++ bot.js
@@ -6,9 +6,32 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+    if (message.content === "Hello") {
+        message.channel.send('Hello, my name is Tommy and I am a fucking shit mid laner.');
     }
+    if (message.content === "oid") {
+        message.channel.send('OOOOID OCROPP!!');
+    }
+    if (message.content === "Fizz") {
+        message.channel.send('CHE SCHIFO DI CHAMP!');
+    }
+
+    if (message.content === "Illaoi") {
+        message.channel.send('I SWEAR SHE IS SO FKIN BROKEN');
+    }
+
+    if (message.content.startsWith("Lee")) {
+        message.channel.send('LI SINNEEEEEEEEE');
+    }
+    if (message.content === `!server`) {
+        message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    }
+
+    if (message.content == "Tommy is bad") {
+        message.react('😄');
+        message.channel.send("That's not even fun.")
+    }
+    console.log(message.content);
 });
 
 client.login(process.env.BOT_TOKEN);
