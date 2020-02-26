a04c62057b9738aa81fff85d8f1aeacbc3e22939
Alessandro <a.buonerba@hotmail.com>
2020-02-26 21:53:17 +0000

    Canvas Add

--- bot.js
+++ bot.js
@@ -6,6 +6,50 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+const applyText = (canvas, text) => {
+    const ctx = canvas.getContext('2d');
+    let fontSize = 70;
+    do {
+        ctx.font = `${fontSize -= 10}px sans-serif`;
+    } while (ctx.measureText(text).width  > canvas.width - 300);
+    return ctx.font;
+}
+
+client.on('guildMemberAdd', async member => {
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+    if (!channel) return;
+    const canvas = Canvas.createCanvas(700, 250);
+    const ctx = canvas.getContext('2d');
+    const background = await Canvas.loadImage('./background.jpg');
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = '#74037b';
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+ctx.font = '28px sans-serif';
+ctx.fillStyle = '#ffffff';
+ctx.fillText('Welcome to the server', canvas.width / 2.5, canvas.height / 1.8);
+    ctx.font = applyText(canvas, member.displayName);
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(member.displayName, canvas.width / 2.5, canvas.height / 1.8);
+
+    ctx.beginPath();
+    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+    
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+    ctx.drawImage(avatar, 25, 0, 200, canvas.height - 50)
+
+    const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png')
+
+    channel.send(`Welcome to the server, ${member}. My name is Little Tommy and I am a real clone of the legendary Tommy. The most terrible player you will ever see!`, attachment);
+});
+
+client.on('message', async message => {
+    if (message.content === "!join") {
+        client.emit('guildMemberAdd', message.member || await message.guild.guild.fetchMember(message.author));
+    }
+});
+
 client.on('message', message => {
     if (message.content === "!quote") {
        let random = Math.floor((Math.random() * messages.length));
@@ -37,8 +81,6 @@ client.on('message', message => {
         message.react('😄');
         message.channel.send("That's not even fun.")
     }
-
-    
 
     if (message.content == "!react") {
         message.react('😄');
