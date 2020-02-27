eddd7350a348b1b91f8e89b19c8df531414b0d70
Alessandro <a.buonerba@hotmail.com>
2020-02-27 13:16:26 +0000

    Fix Typo

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', async messages => {
+client.on('message', async message => {
 if (message === "!cat") {
     let msg = await message.channel.send("Generating a cat meme for all you Tommy's followers");
     
@@ -26,50 +26,6 @@ if (message === "!cat") {
         message.chat.send({embed: cEmbed});
         msg.delete();
 }
-});
-
-const applyText = (canvas, text) => {
-    const ctx = canvas.getContext('2d');
-    let fontSize = 70;
-    do {
-        ctx.font = `${fontSize -= 10}px sans-serif`;
-    } while (ctx.measureText(text).width  > canvas.width - 300);
-    return ctx.font;
-}
-
-client.on('guildMemberAdd', async member => {
-    const channel = member.guild.channels.find(ch => ch.name === 'general');
-    if (!channel) return;
-    const canvas = Canvas.createCanvas(700, 250);
-    const ctx = canvas.getContext('2d');
-    const background = await Canvas.loadImage('./background.jpg');
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = '#74037b';
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    ctx.font = '28px sans-serif';
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText('Welcome to the server', canvas.width / 2.5, canvas.height / 1.8);
-    ctx.font = applyText(canvas, member.displayName);
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText(member.displayName, canvas.width / 2.5, canvas.height / 1.8);
-
-    ctx.beginPath();
-    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.clip();
-    
-    const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
-    ctx.drawImage(avatar, 25, 0, 200, canvas.height - 50)
-
-    const attachment = new Discord.Attachment(canvas.toBuffer(), './background.jpg')
-
-    message.channel.send(`Welcome to the server, ${member}. My name is Little Tommy and I am a real clone of the legendary Tommy. The most terrible player you will ever see!`, attachment);
-});
-
-client.on('message', async message => {
-    if (message.content === "!join!") {
-        client.emit('guildMemberAdd', message.member || await message.guild.fetchUser(message.author));
-    }
 });
 
 client.on('message', message => {
