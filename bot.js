23631e6b41bc26a9b064c6a618282b38b3d727fa
Alessandro <a.buonerba@hotmail.com>
2020-02-27 14:07:50 +0000

    Api Change

--- bot.js
+++ bot.js
@@ -12,8 +12,8 @@ client.on('message', async message => {
 if (message.content === "!cat") {
     let msg = await message.channel.send("Generating a cat meme for all you Tommy's followers");
     
-    let { status } = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json());
-    console.log(status);
+    let { test } = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json());
+    console.log(test.message);
     if(!{test}) return message.channel.send("I am a little broken Tommy. Try again.")
       /*/ let embed = new RichEmbed()
         .setColor(colors.pink)
