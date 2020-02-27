4188c63d5ebd00425d2c64c6f30a47b6ea3cae52
Alessandro <a.buonerba@hotmail.com>
2020-02-27 15:52:21 +0000

    Api Change

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-const { RichEmbed } = require('discord.js');
+const { Client, RichEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 const colors = require('./colors.json');
-const client = new Discord.Client();
+const client = new Client();
 const messages = ['Che schifo di champ', 'Snipered!', 'We need to do fucking JAVA COURSEWORK', 'I will fist your pussy', 'I was predicting.', 'Oid', 'Amir, you are so fucking shit', 'I am good with Zed', 'I do not need Support items', 'I am not drunk', 'Alessaaaaaaaaa', 'Guys, your BOT is MID!!!', 'DO NOT FARM MY MINIONS!!!!', 'DO NOT PUSH MY LANEEE!!!', 'I just woke up', 'HE IS FULL LIFE!!!'];
 
 client.on('ready', () => {
